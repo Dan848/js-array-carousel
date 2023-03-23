a0c006ae4f2e70dec560c530e6e4e6ab03ba962f
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-23 18:37:51 +0100

    add carousel title

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 //Dato un array contenente una lista di cinque immagini, creare un carosello.
 
+
 //FUNZIONI
+
+//Funzione per aggiungere un array di classi a un elemento
+function toggleClasses (element, classes) {
+    for (const _class of classes) {
+      element.classList.toggle(_class);
+  }
+  }
+  
+//Funzione per creare un tag HTML, assegnargli un array di classi e stampare al suo interno del testo
+function createChild(tagName, classes, text) {
+    const newElement = document.createElement(tagName);
+    newElement.innerHTML = text;
+    toggleClasses(newElement, classes);
+    return newElement;
+  }
 
 //BUTTON NEXT
 function goNext () {
@@ -31,31 +47,56 @@ function goPrev () {
 }
 
 function removeActive () {
+    document.querySelectorAll(".carousel-title")[imgIndex].classList.remove("active");
     document.querySelectorAll(".thumb")[imgIndex].classList.remove("active");
     document.querySelectorAll(".slide")[imgIndex].classList.remove("active");
 }
 
 function addActive () {
+    document.querySelectorAll(".carousel-title")[imgIndex].classList.add("active");
     document.querySelectorAll(".thumb")[imgIndex].classList.add("active");
     document.querySelectorAll(".slide")[imgIndex].classList.add("active");
 }
 
 let intervalGoNext;
 function startAutoplay() {
-    intervalGoNext = setInterval (goNext, 3000);
+    intervalGoNext = setInterval (goNext, 4000);
 }
 
 function stopAutoplay () {
-    clearInterval (intervalGoNext);
+    clearInterval(intervalGoNext);
+}
+
+function addImages(element) {
+    carousel.parentNode.insertBefore(createChild("h1", ["carousel-title", "mb-5"], `${element.imgName}`), carousel);
+    slides += `<div class="slide">
+    <img src="./${element.src}" alt="Carosello-${element.imgName}"></div>`;
+    thumb += `<div class="thumb">
+    <img src="./${element.src}" alt="Thumbnail-${element.imgName}"></div>`;
 }
 
 //Creo l'array contenente il nome del file img
 const images = [
-    `img/01.webp`,
-    `img/02.webp`,
-    `img/03.webp`,
-    `img/04.webp`,
-    `img/05.webp`
+    {
+        src: `img/01.webp`,
+        imgName: `Spiderman dal dubbio colore`
+    },
+    {
+        src: `img/02.webp`,
+        imgName: `Ratchet & Clank Rift Apart: SOLO €80 su Amazon...solo eh`
+    },
+    {
+        src: `img/03.webp`,
+        imgName: `Credo sia Fortnite, non ci gioco...ma perché c'è Darth Fener`
+    },
+    {
+        src: `img/04.webp`,
+        imgName: `Stray, bellissimo gioco, bellissimo gatto`
+    },
+    {
+        src: `img/05.webp`,
+        imgName: `Gli Avengers che hai ordinato su Wish... volevo dire della Marvel`
+    }
 ];
 //Associo una variabile css alla lunghezza dell'array per suddividere equamente le immagini nel box thumbnails
 document.documentElement.style.setProperty('--images-number', images.length);
@@ -64,18 +105,24 @@ document.documentElement.style.setProperty('--images-number', images.length);
 const slider = document.querySelector(".slider");
 const thumbnails = document.querySelector(".thumbnails");
 const carousel = document.querySelector(".full-carousel");
+
 let slides = ``;
 let thumb = ``;
 let imgIndex = 0;
+let titleImg = ``;
 
-//Ciclo For per inserire automaticamente le immagini nelle rispettive variabili...
-for (let i = 0; i < images.length; i++) {
-    slides += `<div class="slide">
-    <img src="./${images[i]}" alt="Carosello-${i}"></div>`;
-    thumb += `<div class="thumb">
-    <img src="./${images[i]}" alt="Thumbnail-${i}"></div>`;
-}
+//Ciclo forEach per inserire automaticamente le immagini nelle rispettive variabili...
+
+images.forEach(addImages)
+// for (let i = 0; i < images.length; i++) {
+//     slides += `<div class="slide">
+//     <img src="./${images[i].src}" alt="Carosello-${i}"></div>`;
+//     thumb += `<div class="thumb">
+//     <img src="./${images[i].src}" alt="Thumbnail-${i}"></div>`;
+// }
+console.log(slides)
 //...che andranno poi "aggiunte" ai rispettivi container
+
 slider.innerHTML += slides;
 thumbnails.innerHTML += thumb;
 
